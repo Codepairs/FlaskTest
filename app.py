40f985a0c1bdeacd98c978d2879759a9c9215568
tssvett <tssvett@mail.ru>
2024-02-02 22:39:18 +0300

    test

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request
 from datetime import datetime
-from flask_sqlalchemy import SQLAlchemy
+
+import templates.database
 
 app = Flask(__name__)
-db = SQLAlchemy(app)
-
 
 class FlaskPoweredServer:
     def __init__(self):
-        self.db = db
+        self.db: templates.database.Database|None = None
         self.app = app
         self.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db/base.db'
 
@@ -17,6 +16,32 @@ class FlaskPoweredServer:
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             return render_template('index.html', current_time=current_time)
 
+        @app.route('/test', methods=['GET', 'POST'])
+        def authorization():
+
+            if request.method == 'POST':
+                Login = request.form.get('Login')
+                Password = request.form.get('Password')
+                print(Login, Password)
+                #self.db.insert_time(Password)
+
+
+                if Login == "admin" and Password == "admin":
+                    return "Correct"
+                else:
+                    return "Incorrect"
+
+            return '''
+                     <form method="POST">
+                         <div><label>Login: <input type="text" name="Login"></label></div>
+                         <div><label>Password: <input type="text" name="Password"></label></div>
+                         <input type="submit" value="Enter">
+                     </form>'''
+
+
     def run(self, debug_mode=True):
         self.app.run(debug=debug_mode)
+
+    def set_db(self, db):
+        self.db = db
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from app import *
+from templates.database import Database
 
 
 if __name__ == '__main__':
     app = FlaskPoweredServer()
     app.run(debug_mode=True)
+    db = Database()
+    app.set_db(db)
